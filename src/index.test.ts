3e90cd8b9bb5a1589663d5489dc62ae2c113a0c1
Igor Mucsicska <mucsi_96@yahoo.com>
2019-11-07 23:50:29 +0100

    Initial implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,45 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { createSharedState } from '.';
 
-const render = () => {
-  const useSharedState = createSharedState();
+const render = <ValueType>(initialState: ValueType | undefined = undefined) => {
+  const useSharedState = createSharedState<ValueType | undefined>(initialState);
   return renderHook(() => useSharedState());
 };
 
 describe('createSharedState', () => {
-  it('rocks', () => {
+  it('returns undefined if initial state is not set', () => {
     const { result } = render();
-    expect(result.current).toEqual('test');
+    const [value] = result.current;
+    expect(value).toBe(undefined);
+  });
+
+  it('returns initial state', () => {
+    const { result } = render('initial value');
+    const [value] = result.current;
+    expect(value).toBe('initial value');
+  });
+
+  it('returns set state', () => {
+    const { result } = render('initial value');
+    const [, setValue] = result.current;
+
+    act(() => setValue('set value'));
+
+    const [value] = result.current;
+
+    expect(value).toBe('set value');
+  });
+
+  it('returns set state in other component', () => {
+    const useSharedState = createSharedState('initial value');
+    const { result: resultA } = renderHook(() => useSharedState());
+    const { result: resultB } = renderHook(() => useSharedState());
+    const [, setValue] = resultA.current;
+
+    act(() => setValue('set value from A'));
+
+    const [value] = resultB.current;
+
+    expect(value).toBe('set value from A');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export function createSharedState() {
-  return useState;
+export function createSharedState<ValueType>(initialState: ValueType) {
+  type TListener = (value: ValueType) => void;
+
+  const listeners: TListener[] = [];
+
+  return (): [ValueType, React.Dispatch<React.SetStateAction<ValueType>>] => {
+    const [value, setValue] = useState<ValueType>(initialState);
+
+    useEffect(() => {
+      listeners.forEach(listener => listener !== setValue && listener(value));
+    }, [value]);
+
+    useEffect(() => {
+      listeners.push(setValue);
+
+      return () => {
+        const index = listeners.findIndex(listener => listener === setValue);
+        listeners.splice(index, 1);
+      };
+    }, []);
+
+    return [value, setValue];
+  };
 }
